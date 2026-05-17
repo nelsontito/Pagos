6965674113ef4822c56c54c69c462f7da7b0c444
Nelson Edwin Tito Gironda <nelsontitexx@gamail.com>
2026-05-17 00:24:06 -0400

    17/05/26CreacionMicroservicio

---
+++ CapaPresentacion/Api/EstudiantesController.cs
@@ -0,0 +1,41 @@
+﻿using CapaEntida.DTOs;
+using CapaEntida.Responses;
+using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CapaPresentacion.Api
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    // 2. Definimos la ruta base para este controlador
+    [RoutePrefix("api/estudiantes")]
+    public class EstudiantesController : ApiController
+    {
+
+        // (Opcional) Aquí mismo podrías poner un [HttpGet] para listar clientes si lo necesitas
+        [HttpPost]
+        [Route("consulta")]
+        public IHttpActionResult ConsultaMensualidad(RequestDTO request)
+        {
+            if (request == null || string.IsNullOrEmpty(request.Codigo))
+            {
+                var respuestaError = new Respuesta<List<ConsulMenDTO>>
+                {
+                    Estado = false,
+                    Mensaje = "No se obtuvo datos del codigo QR.",
+                    Data = null
+                };
+
+                return Ok(respuestaError);
+            }
+
+            var respuesta = NInscripciones.GetInstance().ConsultaMensualidad(request.Codigo);
+            return Ok(respuesta);
+        }
+    }
+}
---
+++ CapaPresentacion/App_Start/WebApiConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CapaPresentacion.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // 1. Configuración de CORS (Permite peticiones desde la App de React Native)
+            // El asterisco (*) significa que permites peticiones de cualquier origen, cabecera y método.
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+            // 2. Habilitar rutas por Atributos (Ej: [Route("api/clientes")])
+            config.MapHttpAttributeRoutes();
+
+            // 3. Ruta por defecto de la API
+            // Te sugiero agregar {action} para poder tener varios métodos en un mismo controlador
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                //routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // 3. Opcional: Forzar a que responda en JSON y no en XML
+            //var formatter = config.Formatters.JsonFormatter;
+            //formatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //config.EnsureInitialized();
+        }
+    }
+}
